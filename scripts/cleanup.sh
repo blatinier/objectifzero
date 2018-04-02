ac09504ca1eb0b4cdfcd360c62e6b38f86c12437
Benoît Latinier <benoit@latinier.fr>
2018-04-02 23:57:31 +0200

    Drop unused files

--- scripts/cleanup.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-find . -name \*pyc  | xargs  rm -fv
-find . -name \*pyo | xargs  rm -fv
-find . -name \*~  | xargs  rm -fv
-find . -name __pycache__  | xargs  rm -rfv
-rm -rf coverage .coverage .cache
-
-
