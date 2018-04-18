9478575fe1c1e02dc8dc2941f925d477799a27a1
Fabien Bourrel <fabien.bourrel@hotmail.fr>
2018-04-18 12:22:32 +0200

    Remove unused code

--- src/teams/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-from users.models import User
-
-
-class Team(models.Model):
-    owner = models.ForeignKey(User, on_delete=models.CASCADE,
-                              related_name='team_owner')
-    members = models.ManyToManyField(User, related_name='team_members')
